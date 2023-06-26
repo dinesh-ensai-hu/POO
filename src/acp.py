0127f9e556297face0de1fe0d352f277b5fc600c
Dinesh <dinesh.sivagourou@gmail.com>
2023-06-26 18:36:48 +0200

    New Feature PCA

---
+++ src/acp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn import decomposition
+
+
+iris = datasets.load_iris()
+
+pca = decomposition.PCA()
+pca.fit(iris.data)
